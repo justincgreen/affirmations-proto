e44c0b5ee8c240976950502bb824341913331d1d
Justin Green <justingreendev@gmail.com>
2023-07-07 20:18:05 -0500

    Intial files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+import './styles/app.scss';
+
+function App() {
+  return (
+    <>
+      <h1>Digital Affirmations</h1>
+    </>
+  )
+}
+
+export default App
